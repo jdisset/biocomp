883302570f81a6cb58ad4aa13ab428e97fd7b5f6
Jean Disset <j.disset@gmail.com>
2022-12-22 01:54:44 -0500

    Ready to train on synthetic data

--- scripts/24-train_synth.py
+++ scripts/24-train_synth.py
@@ -80,15 +80,14 @@ node_impl = dict(
 )
 cfg = {
     "optimizer": "adam",
-    "learning_rate": 0.0001,
-    "adam_w_decay": 0.0001,
+    "learning_rate": 3e-4,
     "rng_key": np.random.randint(0, 2**32),
     # "rng_key": 11325,
     "epochs": 200,
     "compile_training": True,
     "batch_size": 8,
-    "norm_factor": 1e7,
-    "balance_bin_resolution": 0.5,
+    "norm_factor": 1,
+    "balance_bin_resolution": 0.2,
     "balance_threshold_quantile": 0.4,
     "balance_threshold_min": 40,
     "node_impl": node_impl,
@@ -112,7 +111,7 @@ models = xp.get_models(node_impl=cfg['node_impl'])
 fwd_models = xp.get_models(node_impl=bc.nodes.DEFAULT_COMPUTE_NODES_DICT, inverse=False, numeric_inputs=True)
 
 zerorng = jax.random.PRNGKey(0)
-ikeys = jax.random.split(zerorng, len(models))
+ikeys = jax.random.split(rng, len(models))
 params, constraints = {}, {}
 for (s, m), r in zip(fwd_models.items(), ikeys):
     params, constraints = m.init(r, pre_params=params, pre_constraints=constraints)
@@ -146,29 +145,62 @@ x_batches, y_batches = du.make_batches_uniform_sampling(Y_list, cfg['batch_size'
 ## ─────────────────────────────────────────────────────────────────────────────
 
 ## ───────────────────────────────────── ▼ ─────────────────────────────────────
+# {{{                        --     stat tools     --
+# ···············································································
+def models_data_fig(models, Y):
+    for sample, model in models.items():
+        out_proteins = model.get_output_proteins()
+        in_proteins = model.get_inverted_input_proteins()
+        z_prot = set(out_proteins) - set(in_proteins)
+        if len(out_proteins) >= 4 and len(out_proteins) <= 5:
+            fig, ax = du.model_heatmap(model, Y[sample])
+        else:
+            fig, ax = du.model_parallel_coords(model, Y[sample])
+        yield sample, model, fig, ax
+
+
+@partial(jit, static_argnums=(1,))
+def compstats(v, smooth_win=1):
+    medians = vmap(jnp.median)(v)
+    mins = vmap(jnp.min)(v)
+    maxs = vmap(jnp.max)(v)
+    p20s = vmap(lambda x: jnp.percentile(x, 20))(v)
+    p80s = vmap(lambda x: jnp.percentile(x, 80))(v)
+    if smooth_win > 1:
+        medians = jnp.convolve(medians, jnp.ones(smooth_win) / smooth_win, mode='same')
+        p80s = jnp.convolve(p80s, jnp.ones(smooth_win) / smooth_win, mode='same')
+        p20s = jnp.convolve(p20s, jnp.ones(smooth_win) / smooth_win, mode='same')
+        maxs = jnp.convolve(maxs, jnp.ones(smooth_win) / smooth_win, mode='same')
+        mins = jnp.convolve(mins, jnp.ones(smooth_win) / smooth_win, mode='same')
+    return medians, p20s, p80s, mins, maxs
+
+
+def get_epoch_stats(epoch_data, smooth_win=1):
+    stats = {'grad': {}, 'params': {}}
+    for k, v in epoch_data['grad']['shared'].items():
+        stats['grad'][k] = compstats(v, smooth_win=smooth_win)
+    for k, v in epoch_data['params']['shared'].items():
+        stats['params'][k] = compstats(v, smooth_win=smooth_win)
+    return stats
+
+
+#                                                                            }}}
+## ─────────────────────────────────────────────────────────────────────────────
+
+## ───────────────────────────────────── ▼ ─────────────────────────────────────
 # {{{                         --     training     --
 # ···············································································
 import wandb as wb
 
-project = 'bp_train_full_02'
-# project = None
+project = 'train_synth_00'
+log_grads_and_params_to_wandb = False
 
 if project is not None:
     wb.init(config=cfg, project=project, entity="jdisset", reinit=True)
 
 print(f"About to train {len(models)} models.")
 
-
-# we take random samples to plot
-# nsubsamples = 1000
-# X_subsamples = {k: jax.random.choice(rng, v, (nsubsamples,)) for k, v in X.items()}
-# let's make it so that we can also take the same samples for Y:
-
 zero_rng = jax.random.PRNGKey(0)
-
-# indices = {k: jax.random.choice(zero_rng, v.shape[0], (nsubsamples,)) for k, v in X.items()}
-# X_samples = {k: v[indices[k]] for k, v in X.items()}
-# Y_samples = {k: v[indices[k]] for k, v in Y.items()}
 
 jitted_models = {
     s: jit(jax.vmap(partial(m, rng_key=zero_rng), in_axes=(None, 0))) for s, m in models.items()
@@ -189,6 +221,7 @@ def local_save(epoch, cfg, epoch_history=None, **_):
         print(f"Done")
     stats = get_epoch_stats(epoch_history)
     du.save(stats, f'{save_dir}/epoch_{epoch}_stats.pkl')
+    du.save(generator_params, f'{save_dir}/generator_params.pkl')
     print(f"Done in {time.time() - t0:.2f}s")
 
 
@@ -221,21 +254,27 @@ def wandb_plot_pred(epoch, cfg, models, X, Y, epoch_history=None, nbatches=len(x
 
 
 def wandb_log_epoch(epoch, cfg, epoch_history=None, nbatches=len(x_batches), **_):
-    if epoch_history is not None:
-        print(f"Logging epoch {epoch} to wandb")
-        # measure time now:
-        t0 = time.time()
-        losses = np.array(epoch_history['loss'])
-        for loss in losses:
-            wb.log({'loss': loss})
-        del losses
-        print(f"Done")
+    if epoch == 0:
+        wb.log({'config': cfg})
+        wb.log({'generator_params': generator_params})
+
+    if epoch_history is None:
+        return
+
+    print(f"Logging epoch {epoch} to wandb")
+    # measure time now:
+    t0 = time.time()
+    losses = np.array(epoch_history['loss'])
+    for loss in losses:
+        wb.log({'loss': loss})
+    del losses
+    if log_grads_and_params_to_wandb:
         stats = du.load(f'{save_dir}/epoch_{epoch}_stats.pkl')
         for k, v in stats['grad'].items():
             wb.log({f'grad/{k}': v})
         for k, v in stats['params'].items():
             wb.log({f'params/{k}': v})
-        print(f"Logging epoch {epoch} to wandb took {time.time() - t0:.2f}s")
+    print(f"Logging epoch {epoch} to wandb took {time.time() - t0:.2f}s")
 
 
 def console_log(epoch, cfg, epoch_history=None, **_):
