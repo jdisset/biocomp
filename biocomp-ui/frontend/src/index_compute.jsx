72c17c3bee32346c0f49019bf5454b9e9efe0e57
Jean Disset <j.disset@gmail.com>
2024-02-08 01:09:12 -0500

    Trimming up big monorepo: moved biocomp-ui

--- biocomp-ui/frontend/src/index_compute.jsx
+++
@@ -1,8 +0,0 @@
-import "regenerator-runtime/runtime";
-import React from "react";
-import ReactDOM from "react-dom";
-import NedComponent from "./NedComponent";
-import ComputeComponent from "./ComputeComponent.jsx";
-import "./style.css";
-
-ReactDOM.render(<ComputeComponent />, document.getElementById("root"));
--- biocomp-ui/frontend/tracer/templates/data_template.js
+++
@@ -1,2 +0,0 @@
-export var layoutData = {{ layoutData }};
-export var pointData = {{ pointData }};
