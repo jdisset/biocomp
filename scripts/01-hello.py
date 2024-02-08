6dc81ba7a5c5d9bf77521f14c0ab3cd7adf97824
Jean Disset <j.disset@gmail.com>
2024-02-08 01:26:26 -0500

    Trimming up big monorepo: moved scripts and tools

--- scripts/01-hello.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-# Create a figure of size 8x6 inches, 80 dots per inch
-plt.figure(figsize=(8, 6), dpi=80)
-x = np.linspace(-np.pi, np.pi, 256, endpoint=True)
-y = np.cos(x)
-plt.plot(x, y)
-
