8fe433263622b8cf1a0a1e868283019af7fc2380
Jean Disset <jdisset@mit.edu>
2025-10-16 20:18:55 -0400

    added dummy version of neural nodes

--- biocomp/config/__init__.py
+++ biocomp/config/__init__.py
@@ -3,3 +3,4 @@ import dracon as dr
 BIOCOMP_COLORS = dr.load("pkg:biocomp:config/biocomp_colors.yaml")
 DEFAULT_COMPUTE_CONFIG = dr.load("pkg:biocomp:config/default_compute_config.yaml")
 DEFAULT_DATA_CONFIG = dr.load("pkg:biocomp:config/default_data_config.yaml")
+SIMPLE_NODES_COMPUTE_CONFIG = dr.load("pkg:biocomp:config/simple_nodes_compute_config.yaml")
